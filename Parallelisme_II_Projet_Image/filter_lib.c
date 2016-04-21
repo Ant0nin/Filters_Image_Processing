497365caa0faa576eff516bd6fe0ad0ee4a43700
Antonin Bernardin <antonin.bernardin@gmail.com>
2016-04-21 21:34:00 +0200

    Résolution d'un bug dans l'application d'un filtre. Pb de taille mémoire pour les canaux RGB lors de la convolution

--- Parallelisme_II_Projet_Image/filter_lib.c
+++ Parallelisme_II_Projet_Image/filter_lib.c
@@ -4,36 +4,44 @@
 
 void applyFilter(PPMImage *image, Filter *filter)
 {
-	int halfW = (filter->w - 1) >> 1;
-	int halfH = (filter->h - 1) >> 1;
+	const int halfW = (filter->w - 1) >> 1;
+	const int halfH = (filter->h - 1) >> 1;
 	int divisionFactor = 0;
-	PPMPixel total;
+	int tempPixel[3]; // RGB
 
 	PPMImage *bufferImage = clonePPM(image);
 
-	for (int x = 0; x < filter->w; x++)
-		for (int y = 0; y < filter->h; y++) {
+	for (int y = 0; y < filter->h; y++) {
+		for (int x = 0; x < filter->w; x++)
 			divisionFactor += filter->data[x + y*filter->w];
 		}
 
-	for (int x = halfW; x <= (image->w - halfW); x++)
-		for (int y = halfH; y <= (image->h - halfW); y++)
+	for (int y = halfH; y < (image->h - halfW); y++)
+		for (int x = halfW; x < (image->w - halfW); x++)
 		{
 			int offsetFilter = 0;
-			total.r = 0; total.g = 0; total.b = 0;
+			tempPixel[0] = 0;
+			tempPixel[1] = 0;
+			tempPixel[2] = 0;
 
 			for (int j = -halfH; j <= halfH; j++) 
 				for (int i = -halfW; i <= halfW; i++)
 				{
 					int offsetImage = (x + image->w * y) + (i + image->w * j);
-					total.r += bufferImage->data[offsetImage].r * filter->data[offsetFilter];
-					total.g += bufferImage->data[offsetImage].g * filter->data[offsetFilter];
-					total.b += bufferImage->data[offsetImage].b * filter->data[offsetFilter];
+					tempPixel[0] += bufferImage->data[offsetImage].r * filter->data[offsetFilter]; // R
+					tempPixel[1] += bufferImage->data[offsetImage].g * filter->data[offsetFilter]; // G
+					tempPixel[2] += bufferImage->data[offsetImage].b * filter->data[offsetFilter]; // B
 					offsetFilter++;
 				}
-			total.r /= divisionFactor;
-			total.g /= divisionFactor;
-			total.b /= divisionFactor;
+			tempPixel[0] /= divisionFactor;
+			tempPixel[1] /= divisionFactor;
+			tempPixel[2] /= divisionFactor;
+
+			PPMPixel total;
+			total.r = tempPixel[0];
+			total.g = tempPixel[1];
+			total.b = tempPixel[2];
+
 			image->data[x + image->w * y] = total;
 		}
 	
